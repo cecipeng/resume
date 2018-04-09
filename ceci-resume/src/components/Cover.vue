8300fea3b9cec5deec10ea7da551cf467fff1a80
seazy peng <1047328479@qq.com>
2018-04-09 18:20:20 +0800

    init

---
+++ ceci-resume/src/components/Cover.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="resume-cover">
+    re
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Cover',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
