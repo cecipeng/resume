e31deec78d5a6322ad75706dcff49deba67bd5e3
seazy peng <1047328479@qq.com>
2018-04-10 12:00:46 +0800

    add cover.vue

--- ceci-resume/src/router/index.js
+++ ceci-resume/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Cover from '@/components/Cover'
+import Animation from '@/components/Animation'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'Cover',
       component: Cover
+    },
+    {
+      path: '/',
+      name: 'Animation',
+      component: Animation
     }
   ]
 })
